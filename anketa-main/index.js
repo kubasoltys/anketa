9770f4530b7b322b3c78208f757437f2d20fcc70
kubasoltys <120708238+kubasoltys@users.noreply.github.com>
2024-03-26 13:03:02 +0100

    Add files via upload

---
+++ anketa-main/index.js
@@ -0,0 +1,62 @@
+// připojení modulu Express
+const express = require('express');
+const app = express();
+console.log("page is active");
+
+// připojení modulu Body-parser
+const bodyParser = require('body-parser');
+
+// idk
+const fs = require('fs');
+
+
+// aktivování složky s css a img
+app.use(express.static('public'));
+
+// aktivování ejs
+app.set('view engine', 'ejs');
+
+// idk
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// nastavení domovské stránky
+app.get('/', function (req, res) {
+    res.render('index.ejs');
+})
+
+// nastavení další stránky
+app.get('/about', function (req, res) {
+    res.render('about');
+})
+
+// nastavení další stránky
+app.get('/results', function (req, res) {
+    res.render('results');
+})
+
+// zpracování zadaných informací a uložení do objektu subject
+app.post('/saved', function (req, res) {
+    let subject = {
+        name: req.body.jmeno,
+        favouriteSubject: req.body.predmet,
+        time: new Date().toISOString(),
+        ip: req.ip.split(':').pop()
+    }
+
+    // Čtení stávajících dat z souboru
+    fs.readFile("saved/responses.json", function (err, data) {
+        if (err) throw err;
+        let json = JSON.parse(data);
+        json.push(newResponse);
+    })
+
+    // idk
+    fs.writeFile('saved/responses.json', JSON.stringify(json, null, 2), function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+        res.redirect('results');
+    });
+})
+
+// port 3001
+app.listen(3001);
